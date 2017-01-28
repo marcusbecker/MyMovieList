a81bf68d07ee9a49e18d3db55c661d97b175d0cc
Marcus Becker <marcusvbecker@gmail.com>
2017-01-28 13:07:57 -0200

    Layout landscape mode

--- app/src/main/java/br/com/mvbos/mml/MovieDetailActivity.java
+++ app/src/main/java/br/com/mvbos/mml/MovieDetailActivity.java
@@ -75,7 +75,7 @@ public class MovieDetailActivity extends AppCompatActivity implements LoaderMana
 
             title.setText(movieSelected.getTitle());
             release.setText(movieSelected.getReleaseDate());
-            vote.setText(String.format(Locale.ENGLISH, "%.1f/10", movieSelected.getRating()));
+            vote.setText(String.format(Locale.ENGLISH, "%.1f / 10", movieSelected.getRating()));
             overview.setText(movieSelected.getOverview());
 
             String path = getString(R.string.image_url_thumb) + movieSelected.getImagePath();
